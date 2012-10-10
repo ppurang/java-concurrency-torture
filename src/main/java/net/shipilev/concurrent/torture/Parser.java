f76e3bfc8e428b440b70752e9ecdffe3ef941823
Aleksey Shipilev <aleksey.shipilev@gmail.com>
2012-10-11 01:44:30 +0400

    Output basic HTML.

--- src/main/java/net/shipilev/concurrent/torture/Parser.java
+++ src/main/java/net/shipilev/concurrent/torture/Parser.java
@@ -14,16 +14,17 @@ import javax.xml.bind.Unmarshaller;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
 
-    private final String resultFile;
+    private final String src;
     private final Map<String, Test> descriptions;
 
-    public Parser(Options opts) throws JAXBException {
-        resultFile = opts.getResultFile();
+    public Parser(Options opts) throws JAXBException, FileNotFoundException {
+        src = opts.getResultFile();
         descriptions = new HashMap<String, Test>();
         readDescriptions();
     }
@@ -37,26 +38,46 @@ public class Parser {
     }
 
     public void parse() throws FileNotFoundException, JAXBException {
-        Results result = unmarshal(Results.class, new FileInputStream(resultFile));
+        PrintWriter output = new PrintWriter("result.html");
+
+        Results result = unmarshal(Results.class, new FileInputStream(src));
 
         for (Result r : result.getResult()) {
 
+            output.println("<p>" + r.getName() + "</p>");
+
             Test test = descriptions.get(r.getName());
             if (test == null) {
-                System.err.println("Missing description for " + r.getName());
+                output.println("Missing description for " + r.getName());
                 continue;
             }
+
+            output.println("<table>");
+            output.println("<tr>");
+            output.println("<th>Observed state</th>");
+            output.println("<th>Occurence</th>");
+            output.println("<th>Outcome</th>");
+            output.println("<th>Interpretation</th>");
+            output.println("</tr>");
 
             for (Case c : test.getCase()) {
                 for (State s : r.getState()) {
                     if (c.getMatch().contains(s.getId())) {
                         // match!
-                        System.err.println(s.getId() + " " + s.getCount() + " " + c.getOutcome() + " " + c.getDescription());
+                        output.println("<tr>");
+                        output.println("<td>" + s.getId() + "</td>");
+                        output.println("<td>" + s.getCount() + "</td>");
+                        output.println("<td>" + c.getOutcome() + "</td>");
+                        output.println("<td>" + c.getDescription() + "</td>");
+                        output.println("</tr>");
                     }
                 }
             }
+            output.println("</table>");
+
         }
 
+        output.close();
     }
 
     public <T> T unmarshal(Class<T> docClass, InputStream inputStream)
