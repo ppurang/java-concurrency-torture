078e6a78b4326e0d3c21eac462a37da3f51d8644
Aleksey Shipilev <aleksey.shipilev@gmail.com>
2012-10-11 21:00:03 +0400

    Other atomicity tests.

---
+++ src/main/java/net/shipilev/concurrent/torture/tests/primitiveAtomicity/CharAtomicityTest.java
@@ -0,0 +1,38 @@
+package net.shipilev.concurrent.torture.tests.primitiveAtomicity;
+
+import net.shipilev.concurrent.torture.tests.OneActorOneObserverTest;
+
+/**
+ * Tests if primitive integers experience non-atomic reads/writes.
+ *
+ * @author Aleksey Shipilev (aleksey.shipilev@oracle.com)
+ */
+public class CharAtomicityTest implements OneActorOneObserverTest<CharAtomicityTest.Specimen> {
+
+    public static class Specimen {
+        char x;
+    }
+
+    @Override
+    public Specimen newState() {
+        return new Specimen();
+    }
+
+    @Override
+    public void actor1(Specimen s) {
+        s.x = (char)0xFFFF;
+    }
+
+    @Override
+    public void observe(Specimen s, byte[] result) {
+        char t = s.x;
+        result[0] = (byte) ((t >> 0) & 0xFF);
+        result[1] = (byte) ((t >> 8) & 0xFF);
+    }
+
+    @Override
+    public int resultSize() {
+        return 2;
+    }
+
+}
---
+++ src/main/java/net/shipilev/concurrent/torture/tests/primitiveAtomicity/ShortAtomicityTest.java
@@ -0,0 +1,38 @@
+package net.shipilev.concurrent.torture.tests.primitiveAtomicity;
+
+import net.shipilev.concurrent.torture.tests.OneActorOneObserverTest;
+
+/**
+ * Tests if primitive integers experience non-atomic reads/writes.
+ *
+ * @author Aleksey Shipilev (aleksey.shipilev@oracle.com)
+ */
+public class ShortAtomicityTest implements OneActorOneObserverTest<ShortAtomicityTest.Specimen> {
+
+    public static class Specimen {
+        short x;
+    }
+
+    @Override
+    public Specimen newState() {
+        return new Specimen();
+    }
+
+    @Override
+    public void actor1(Specimen s) {
+        s.x = (short)0xFFFF;
+    }
+
+    @Override
+    public void observe(Specimen s, byte[] result) {
+        short t = s.x;
+        result[0] = (byte) ((t >> 0) & 0xFF);
+        result[1] = (byte) ((t >> 8) & 0xFF);
+    }
+
+    @Override
+    public int resultSize() {
+        return 2;
+    }
+
+}
